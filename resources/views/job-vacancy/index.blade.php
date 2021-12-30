b593a0094506709e0ff80f9f3e3e8ed903843778
rakhazan <rakhazahnur26@gmail.com>
2021-12-30 07:50:09 +0700

    :lipstick: Add applicants modal

--- resources/views/job-vacancy/index.blade.php
+++ resources/views/job-vacancy/index.blade.php
@@ -10,6 +10,7 @@
         <div class="section-header justify-content-between">
             <h1>Job Vacancy</h1>
             <button class="btn btn-outline-success" type="button" data-toggle="modal" data-target="#applied_job">Applied Job</button>
+            <button class="btn btn-outline-success" type="button" data-toggle="modal" data-target="#applicants">All Applicants</button>
         </div>
 
         <div class="section-body">
@@ -53,7 +54,7 @@
         </div>
     </section>
 
-    {{-- Modal Job Detail --}}
+    {{-- Modal Applied --}}
     <div class="modal fade" id="applied_job" tabindex="-1" role="dialog" aria-labelledby="modalJobDetail" aria-hidden="true">
         <div class="modal-dialog modal-xl" role="document">
             <div class="modal-content">
@@ -92,6 +93,47 @@
             </div>
         </div>
     </div>
-    {{-- End Modal Job Detail --}}
+    {{-- End Modal Applied --}}
+
+    {{-- Modal Applicants --}}
+    <div class="modal fade" id="applicants" tabindex="-1" role="dialog" aria-labelledby="modalJobDetail" aria-hidden="true">
+        <div class="modal-dialog modal-xl" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">All Applicants</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <table class="w-100">
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Email</th>
+                                <th>Gender</th>
+                                <th>Birth Date</th>
+                                <th>Applied</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Injilia Ratuntiga i</td>
+                                <td>injil.r3@gmail.com </td>
+                                <td>Female</td>
+                                <td>17/07/00</td>
+                                <td>Intership</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="modal-footer">
+                    {{-- <button type="button" class="btn btn-primary">APPLY THIS JOB</button> --}}
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    {{-- End Modal Applicants --}}
 
 @endsection
