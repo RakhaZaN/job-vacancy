f8ee4af7c3767df1aa564cbf87c319eba1165710
rakhazan <rakhazahnur26@gmail.com>
2022-01-23 10:34:35 +0700

    hide some menu in side bar for admin

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -11,24 +11,26 @@
             <li class="nav-item dropdown {{ Request::routeIs('home') ? 'active' : '' }}">
                 <a class="nav-link" href="{{ route('home') }}"><i class="fas fa-home"></i> <span>Home</span></a>
             </li>
-            <li class="nav-item dropdown {{ Request::routeIs('aboutus') ? 'active' : '' }}">
-                <a class="nav-link" href="{{ route('aboutus') }}"><i class="fas fa-info-circle"></i> <span>About Us</span></a>
-            </li>
             <li class="nav-item dropdown {{ Request::routeIs('job-vacancy.*') ? 'active' : '' }}">
                 <a class="nav-link " href="{{ route('job-vacancy.index') }}"><i class="fas fa-universal-access"></i> <span>Job Vacancy</span></a>
-            </li>
-            <li class="nav-item dropdown {{ Request::routeIs('hta') ? 'active' : '' }}">
-                <a class="nav-link" href="{{ route('hta') }}"><i class="fas fa-question-circle"></i> <span>How to Apply</span></a>
-            </li>
-            <li class="nav-item dropdown {{ Request::routeIs('news') ? 'active' : '' }}">
-                <a class="nav-link" href="{{ route('news') }}"><i class="fas fa-newspaper"></i> <span>News & Events</span></a>
             </li>
             <li class="nav-item dropdown {{ Request::routeIs('announcement') ? 'active' : '' }}">
                 <a class="nav-link" href="{{ route('announcement') }}"><i class="fas fa-bullhorn"></i> <span>Announcement</span></a>
             </li>
+            <li class="nav-item dropdown {{ Request::routeIs('news') ? 'active' : '' }}">
+                <a class="nav-link" href="{{ route('news') }}"><i class="fas fa-newspaper"></i> <span>News & Events</span></a>
+            </li>
+            @if (auth()->user()->role != 'admin')
+            <li class="nav-item dropdown {{ Request::routeIs('aboutus') ? 'active' : '' }}">
+                <a class="nav-link" href="{{ route('aboutus') }}"><i class="fas fa-info-circle"></i> <span>About Us</span></a>
+            </li>
+            <li class="nav-item dropdown {{ Request::routeIs('hta') ? 'active' : '' }}">
+                <a class="nav-link" href="{{ route('hta') }}"><i class="fas fa-question-circle"></i> <span>How to Apply</span></a>
+            </li>
             <li class="nav-item dropdown {{ Request::routeIs('contact') ? 'active' : '' }}">
                 <a class="nav-link" href="{{ route('contact') }}"><i class="fas fa-phone"></i> <span>Contact</span></a>
             </li>
+            @endif
             @if (auth()->user()->role == 'admin')
             <li class="nav-item dropdown">
                 <a class="nav-link" href="{{ route('admin.register') }}"><i class="fas fa-users"></i> <span>Add Admin</span></a>
